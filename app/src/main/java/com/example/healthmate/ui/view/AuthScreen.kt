0719b71e341e2427963f1e4adf7b4c5a799e6a65
vicki <surajbarik2003@gmail.com>
2024-12-07 02:03:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/healthmate/ui/view/AuthScreen.kt
@@ -0,0 +1,11 @@
+package com.example.healthmate.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun AuthScreen(modifier: Modifier = Modifier) {
+    
+}

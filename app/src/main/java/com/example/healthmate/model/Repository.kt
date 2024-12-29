473ab7d90cef3a9a55e64741540b607c3b11e6d3
vicki <surajbarik2003@gmail.com>
2024-12-29 14:37:42 +0530

    Category and Brand related operations are implemented

--- app/src/main/java/com/example/healthmate/model/Repository.kt
+++ app/src/main/java/com/example/healthmate/model/Repository.kt
@@ -67,4 +67,50 @@ class Repository @Inject constructor(
     } catch (e: Exception){
         Result.failure(e)
     }
+
+    suspend fun getProductsByCategory(category: String): Result<List<Product>> = try {
+        val response = apiService.getProductByCategory(category)
+        if (response.success && response.data != null){
+            Result.success(response.data)
+        } else {
+            Result.failure(Exception(response.message ?: "Unknown error occurred"))
+        }
+    } catch (e: Exception){
+        Result.failure(e)
+    }
+
+    // BRAND RELATED OPERATIONS
+    suspend fun getAllBrands(): Result<List<Brand>> = try {
+        val response = apiService.getAllBrands()
+        if (response.success){
+            Result.success(response.data)
+        } else {
+            Result.failure(Exception("Failed to fetch brands"))
+        }
+    } catch (e:Exception) {
+        Result.failure(e)
+    }
+
+    suspend fun getProductsByBrand(brand: String): Result<List<Brand>> = try {
+        val response = apiService.getProductsByBrand(brand)
+        if (response.success){
+            Result.success(response.data)
+        } else {
+            Result.failure(Exception("Failed to fetch brands"))
+        }
+    } catch (e: Exception){
+        Result.failure(e)
+    }
+
+    // Auth Related Operations
+    suspend fun signup(name: String, phone: String, email: String? = null): Result<String> = try {
+        val response = apiService.signup(SignupRequest(name, phone, email))
+        if (response.success && response.token != null) {
+            Result.success(response.token)
+        } else {
+            Result.failure(Exception(response.error ?: "SignUp Failed"))
+        }
+    } catch (e: Exception){
+        Result.failure(e)
+    }
 }

37133a598847f90959ce288eda23ccdc7fec5d66
vicki <surajbarik2003@gmail.com>
2025-01-02 22:37:24 +0530

    initial implementation

---
+++ app/src/main/java/com/example/healthmate/view/productScreens/Components.kt
@@ -0,0 +1,150 @@
+package com.example.healthmate.view.productScreens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.healthmate.R
+import com.example.healthmate.model.Product
+
+@Composable
+fun ProductCard(
+    modifier: Modifier = Modifier,
+    product: Product,
+    onClick: () -> Unit
+) {
+   Card(
+       modifier = modifier
+           .fillMaxWidth()
+           .clickable(onClick = onClick)
+           .padding(8.dp),
+       shape = RoundedCornerShape(12.dp),
+       elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+   ) {
+       Column {
+           AsyncImage(
+               model = ImageRequest.Builder(context = LocalContext.current)
+                   .data(product.imageLinks.firstOrNull())
+                   .crossfade(true)
+                   .build(),
+               contentDescription = product.name,
+               error = painterResource(R.drawable.ic_broken_image),
+               placeholder = painterResource(R.drawable.loading_img),
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .height(160.dp)
+                   .clip(RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp)),
+               contentScale = ContentScale.Crop
+           )
+           Column(modifier = modifier.padding(12.dp)) {
+               Text(
+                   text = product.name,
+                   style = MaterialTheme.typography.titleMedium,
+                   maxLines = 2,
+                   overflow = TextOverflow.Ellipsis
+               )
+               Spacer(modifier = modifier.height(4.dp))
+
+               Text(
+                   text = "${product.price}",
+                   style = MaterialTheme.typography.titleLarge,
+                   fontWeight = FontWeight.Bold,
+                   color = MaterialTheme.colorScheme.primary
+               )
+               Spacer(modifier = modifier.height(4.dp))
+
+               Text(
+                   text = product.brand,
+                   style = MaterialTheme.typography.bodyMedium,
+                   color = MaterialTheme.colorScheme.onSurfaceVariant
+               )
+           }
+       }
+   }
+}
+
+@Composable
+fun SortingBar(
+    modifier: Modifier = Modifier,
+    onSortChange: (String, String) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        contentAlignment = Alignment.CenterEnd
+    ) {
+        Row(modifier = modifier.clickable { expanded = !expanded },
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Sort")
+            Icon(
+                imageVector = Icons.Default.ArrowDropDown,
+                contentDescription = "Dropdown Icon"
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    onSortChange("asc","price")
+                    expanded = false
+                },
+                text = { Text("Price: Low To High") }
+            )
+
+            DropdownMenuItem(
+                onClick = {
+                    onSortChange("desc","price")
+                    expanded = false
+                },
+                text = { Text("Price: High To Low") }
+            )
+
+            DropdownMenuItem(
+                onClick = {
+                    onSortChange("asc","name")
+                    expanded = false
+                },
+                text = { Text("Name: A_Z") }
+            )
+        }
+    }
+}

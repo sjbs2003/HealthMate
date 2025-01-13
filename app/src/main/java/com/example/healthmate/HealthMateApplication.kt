30419b092439d5ae639a66814c46d48669dccb8a
vicki <surajbarik2003@gmail.com>
2025-01-13 19:44:55 +0530

    updated with Koin's DI from Hilt

--- app/src/main/java/com/example/healthmate/HealthMateApplication.kt
+++ app/src/main/java/com/example/healthmate/HealthMateApplication.kt
@@ -1,7 +1,27 @@
 package com.example.healthmate
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import com.example.healthmate.model.authModule
+import com.example.healthmate.model.networkModule
+import com.example.healthmate.model.repositoryModule
+import com.example.healthmate.viewmodel.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
-@HiltAndroidApp
-class HealthMateApplication : Application()
+
+class HealthMateApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@HealthMateApplication)
+            modules(
+                networkModule,
+                repositoryModule,
+                authModule,
+                viewModelModule
+            )
+        }
+    }
+}

b844ef6f62631e3f599d544dec543d032b8a5fd0
vicki <surajbarik2003@gmail.com>
2025-01-13 19:43:37 +0530

    added repositoryModule and authModule for DI

--- app/src/main/java/com/example/healthmate/model/RepositoryModule.kt
+++ app/src/main/java/com/example/healthmate/model/RepositoryModule.kt
@@ -1,31 +1,12 @@
 package com.example.healthmate.model
 
-import android.content.Context
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
 
-
-@Module
-@InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
-    @Provides
-    @Singleton
-    fun provideRepository(apiService: ApiService): Repository {
-        return Repository(apiService)
-    }
+val repositoryModule = module {
+    single { Repository(get()) }
 }
 
-@Module
-@InstallIn(SingletonComponent::class)
-object AuthModule {
-    @Provides
-    @Singleton
-    fun provideAuthManager(@ApplicationContext context: Context): AuthManager {
-        return AuthManager(context)
-    }
+val authModule = module {
+    single { AuthManager(androidContext()) }
 }

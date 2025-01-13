22d5c156a84f78dced0821899f78bab92ac79856
vicki <surajbarik2003@gmail.com>
2025-01-13 19:42:31 +0530

    added this file for viewmodel's DI

---
+++ app/src/main/java/com/example/healthmate/viewmodel/ViewModelModule.kt
@@ -0,0 +1,9 @@
+package com.example.healthmate.viewmodel
+
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { AuthViewModel(get(), get()) }
+    viewModel { ProductViewModel(get()) }
+}

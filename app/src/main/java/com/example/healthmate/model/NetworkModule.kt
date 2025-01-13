eb27434933c86a4caf13bf036cc131e666fc4771
vicki <surajbarik2003@gmail.com>
2025-01-13 18:32:30 +0530

    updated from Hilt to Koin

--- app/src/main/java/com/example/healthmate/model/NetworkModule.kt
+++ app/src/main/java/com/example/healthmate/model/NetworkModule.kt
@@ -1,32 +1,22 @@
 package com.example.healthmate.model
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import org.koin.dsl.module
 import retrofit2.Retrofit
-import javax.inject.Singleton
 
 
-@Module
-@InstallIn(SingletonComponent::class)
-object NetworkModule {
-
-    @Provides
-    @Singleton
-    fun provideJson(): Json = Json {
-        ignoreUnknownKeys = true
-        coerceInputValues = true
+val networkModule = module {
+    single {
+        Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+        }
     }
-
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-        return OkHttpClient.Builder()
+    single {
+        OkHttpClient.Builder()
             .addInterceptor { chain ->
                 val request = chain.request()
                 println("Making Request: ${request.url}")
@@ -36,20 +26,17 @@ object NetworkModule {
             }
             .build()
     }
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(json: Json, client: OkHttpClient): Retrofit {
-        return Retrofit.Builder()
+    single {
+        val json: Json = get()
+        val client: OkHttpClient = get()
+        Retrofit.Builder()
             .baseUrl("http://10.0.2.2:3001/")
             .client(client)
             .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
-
-    @Provides
-    @Singleton
-    fun provideApiService(retrofit: Retrofit): ApiService {
-        return retrofit.create(ApiService::class.java)
+    single {
+        val retrofit: Retrofit = get()
+        retrofit.create(ApiService::class.java)
     }
 }

ea86e92bde525d6882e8977af7bf669036d82ad7
vicki <surajbarik2003@gmail.com>
2025-01-14 23:02:27 +0530

    updated

--- app/src/main/java/com/example/healthmate/viewmodel/ViewModelModule.kt
+++ app/src/main/java/com/example/healthmate/viewmodel/ViewModelModule.kt
@@ -6,4 +6,5 @@ import org.koin.dsl.module
 val viewModelModule = module {
     viewModel { AuthViewModel(get(), get()) }
     viewModel { ProductViewModel(get()) }
+    viewModel { CartViewModel(get()) }
 }
